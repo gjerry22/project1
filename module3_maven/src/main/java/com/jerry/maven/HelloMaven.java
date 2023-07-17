9937acd12ecab30de1983415ec92c16a7a89cf1e
Jerry <2911377200@qq.com>
2023-07-17 11:43:09 +0800

    first commit

---
+++ module3_maven/src/main/java/com/jerry/maven/HelloMaven.java
@@ -0,0 +1,15 @@
+package com.jerry.maven;
+
+/**
+ * ClassName: HelloMaven
+ * Package: com.jerry.maven
+ * Description:
+ *
+ * @Author: Jerry
+ * @Date: 2023/4/6
+ */
+public class HelloMaven {
+    public String hello(String message) {
+        return message;
+    }
+}
